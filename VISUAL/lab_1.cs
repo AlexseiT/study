717ea6cf433ce74f04c0b87681cf94825d5f87b2
AlexseiT <alex.todorov@mail.ru>
2022-02-16 23:26:56 +0700

    1 лаба по Визуальному прог.

---
+++ VISUAL/lab_1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+public class HW1
+{
+    public static void PrintMass(int[][] A, int n)
+    {
+        Console.WriteLine("--------------");
+        for (int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < A[i].Length; j++)
+            {
+                Console.Write($" {A[i][j]}");
+            }
+            Console.WriteLine("");
+        }
+    }
+    public static long QueueTime(int[] customers, int n)
+    {
+        int[][] array = new int[n][];
+        for (int i = 0; i < n; i++)
+        {
+            if (i == 0) array[i] = new int[customers.Length];
+            else array[i] = new int[1];
+        }
+
+        for (int i = 0; i < customers.Length; i++)
+        {
+            array[0][i] = customers[i];
+        }
+
+        int check = 0;
+        int timer= 0;
+        while (check != n)
+        {
+            timer++;
+
+            PrintMass(array,n);
+
+            check = 0;
+            for (int i = 0; i < n; i++)
+            {
+            if (array[i][0] == 0)
+                {
+                    int s = 1;
+                    while (array[0][s] == 0)
+                    {
+                        s++;
+                        if (s == customers.Length) break;
+                    }
+                    if (s != customers.Length)
+                    {
+                        array[i][0] = array[0][s];
+                        array[0][s] = 0;
+                    }
+                }
+            if (array[i][0] != 0) array[i][0]--;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                if (array[i][0] == 0) check++;
+            }
+            if (check == n) 
+            for (int i = 0; i < customers.Length; i++) if (array[0][i] != 0) check = 0;
+            
+        }
+        return timer;
+    }
+}
+namespace Example
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Количество людей ");
+
+            int kol = int.Parse(Console.ReadLine());
+
+            int[] customers = new int[kol];
+
+            for (int i = 0; i < customers.Length; i++)
+
+            {
+
+                Console.Write($"Время для {i + 1} покупателя: ");
+
+                customers[i] = int.Parse(Console.ReadLine());
+
+            }
+
+            Console.Write("Количество касс: ");
+
+            int n = int.Parse(Console.ReadLine());
+
+            Console.Write($"Время = {HW1.QueueTime(customers, n)}");
+        }
+    }
+}
