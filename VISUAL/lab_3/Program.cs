17faf98629e1c8e6b63f45a2a2bd372ea0295d1f
Kochtyan <messka.sanfirov@inbox.ru>
2022-03-03 22:05:03 +0700

    AlexseiT: +lab3

---
+++ VISUAL/lab_3/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Example
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Тесты");
+            Console.WriteLine();
+
+            RomanNumber Obj1 = new RomanNumber(12);
+            RomanNumber Obj2 = new RomanNumber(230);
+            RomanNumber Obj3 = new RomanNumber(1560);
+
+            Console.WriteLine("12 : XII : " + Obj1.ToString());
+            Console.WriteLine("230 : CCXXX : " + Obj2.ToString());
+            Console.WriteLine("1560 : MDLX : " + Obj3.ToString());
+
+            Console.WriteLine();
+
+            Console.WriteLine("12 + 230 = 242 : CCXLII : " + (Obj1 + Obj2).ToString());
+            Console.WriteLine("1560 - 230 = 1330 : MCCCXXX : " + (Obj3 - Obj2).ToString());
+            Console.WriteLine("12 * 230 = 2760 : MMDCCLX : " + (Obj1 * Obj2).ToString());
+            Console.WriteLine("1560 / 12 = 130 : CXXX : " + (Obj3 / Obj1).ToString());
+
+            Console.WriteLine();
+
+            Console.WriteLine("Массив:");
+            RomanNumber[] mass = { Obj2, Obj3, Obj1};
+            for (int i = 0; i < mass.Length; i++) Console.WriteLine(mass[i]);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Отсортированный Массив:");
+
+            Array.Sort(mass);
+
+            for (int i = 0; i < mass.Length; i++) Console.WriteLine(mass[i]);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Копирование элемента 230 : CCXLII ");
+
+            var Obj4 = (RomanNumber)Obj2.Clone();
+
+            Console.WriteLine(Obj4.ToString());
+
+        }
+    }
+}
---
+++ VISUAL/lab_3/RomanNumberTests.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass()]
+    public class RomanNumberTests
+    {
+        [TestMethod()]
+        public void ToStringTest()
+        {
+         RomanNumber a = new(1);
+         Assert.AreEqual("I", a.ToString());
+         a = new(3999);
+         Assert.AreEqual("MMMCMXCIX", a.ToString());
+        }
+        [TestMethod()]
+        public void Add()
+        {
+            RomanNumber a = new (1);
+            RomanNumber b;
+            RomanNumber c;
+            for (ushort i = 1; i < 3998; i++)
+            {
+
+                b = new (i);
+                c = new (++i);
+                Assert.AreEqual(c.ToString(), (a + b).ToString());
+            }
+        }
+        [TestMethod()]
+        public void Sub()
+        {
+            RomanNumber a = new (1);
+            RomanNumber b;
+            RomanNumber c;
+            for (ushort i = 3999; i >= 2; i--)
+            {
+                b = new (i);
+                c = new (--i);
+                Assert.AreEqual(c.ToString(), (b - a).ToString());
+            }
+        }
+        [TestMethod()]
+        public void Mul()
+        {
+         
+            RomanNumber a = new (2);
+            RomanNumber b;
+            RomanNumber c;
+            for (ushort i = 1; i < 2000 ; i++)
+            {
+                b = new (i);
+                c = new (Convert.ToUInt16(i * 2));
+                Assert.AreEqual(c.ToString(), (a * b).ToString());
+            }
+
+        }
+        [TestMethod()]
+        public void Div()
+        {
+            RomanNumber a = new (2);
+            RomanNumber b;
+            RomanNumber c;
+            for (ushort i = 3998; i >= 2; i-=2)
+            {
+                b = new (i);
+                c = new (Convert.ToUInt16(i / 2));
+                Assert.AreEqual(c.ToString(), (b / a).ToString());
+            }
+        }
+        [TestMethod()]
+        public void CloneTest()
+        {
+            RomanNumber a;
+            for (ushort i = 1; i < 4000; i++)
+            {
+                a = new (i);
+                var c = a.Clone();
+                Assert.AreEqual(a.ToString(), c.ToString());
+            }
+        }
+        [TestMethod()]
+        public void ExceptionTest()
+        {
+            RomanNumber a;
+            RomanNumber b;
+            Assert.ThrowsException<RomanNumberException>(()=>a = new (0));
+            Assert.ThrowsException<RomanNumberException>(()=>a = new (4000));
+            Assert.ThrowsException<RomanNumberException>(()=>a = new (10000));
+            a = new (1);
+            b = new (3999);
+            Assert.ThrowsException<RomanNumberException>(()=>(a + b).ToString());
+            a = new (1);
+            b = new (1);
+            Assert.ThrowsException<RomanNumberException>(()=>(a - b).ToString());
+            a = new (2);
+            b = new (2000);
+            Assert.ThrowsException<RomanNumberException>(()=>(a * b).ToString());
+            a = new (1);
+            b = new (10);
+            Assert.ThrowsException<RomanNumberException>(() => (a / b).ToString());
+        }
+
+        [TestMethod()]
+        public void CompareToTest()
+        {
+            RomanNumber a;
+            RomanNumber b;
+            for (ushort i = 1; i < 4000; i++)
+            {
+                a = new (i);
+                b = new (i);
+                Assert.IsTrue(a.CompareTo(b) == 0);
+            }
+            a = new (1);
+            for (ushort i = 2; i < 4000; i++)
+            {
+                b = new (i);
+                Assert.IsTrue(a.CompareTo(b) != 0);
+            }
+        }
+
+    }
+}
