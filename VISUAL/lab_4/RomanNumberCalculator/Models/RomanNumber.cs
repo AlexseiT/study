fe35c8f4445f29e101c0da528cac37a4d98d07f6
Kochtyan <messka.sanfirov@inbox.ru>
2022-03-10 23:31:09 +0700

    AlexseiT: + lab_4

---
+++ VISUAL/lab_4/RomanNumberCalculator/Models/RomanNumber.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+public class RomanNumber : ICloneable, IComparable
+{
+    private protected ushort ch;
+    //Конструктор получает число n, которое должен представлять объект класса
+    public RomanNumber(ushort n)
+    {
+        if (n > 0 && n < 4000) ch = n;
+        else throw new RomanNumberException("Входные данные неверные");
+    }
+    //Сложение римских чисел
+    public static RomanNumber operator +(RomanNumber? n1, RomanNumber? n2)
+    {
+        if (n1 == null || n2 == null || n1.ch + n2.ch > 3999) throw new RomanNumberException("Неверные параметры");
+        else return new RomanNumber((ushort)(n1.ch + n2.ch));
+    }
+    //Вычитание римских чисел
+    public static RomanNumber operator -(RomanNumber? n1, RomanNumber? n2)
+    {
+        if (n1 == null || n2 == null || n1.ch - n2.ch <= 0) throw new RomanNumberException("Неверные параметры");
+        else return new RomanNumber((ushort)(n1.ch - n2.ch));
+    }
+    //Умножение римских чисел
+    public static RomanNumber operator *(RomanNumber? n1, RomanNumber? n2)
+    {
+        if (n1 == null || n2 == null || n1.ch * n2.ch > 3999 || n1.ch * n2.ch <= 0 ) throw new RomanNumberException("Неверные параметры");
+        else return new RomanNumber((ushort)(n1.ch * n2.ch));
+    }
+    //Целочисленное деление римских чисел
+    public static RomanNumber operator /(RomanNumber? n1, RomanNumber? n2)
+    {
+        if (n1 == null || n2 == null || n2.ch == 0 || n1.ch / n2.ch <= 0) throw new RomanNumberException("Неверные параметры");
+        else return new RomanNumber((ushort)(n1.ch / n2.ch));
+    }
+    //Возвращает строковое представление римского числа
+    public override string ToString()
+    {
+        string ch_string = "";
+
+        char[] Alfavit = { 'M', 'D', 'C', 'L', 'X', 'V', 'I' };
+
+        ushort ch_element = ch;
+
+        int counter = 0;
+
+        while (ch_element != 0)
+        {
+            counter++;
+            ch_element /= 10;
+        }
+
+
+        ch_element = ch;
+        double element = 0;
+        double element_des = 0;
+
+        for (int i = counter - 1; i >= 0; i--)
+        {
+
+            double ch_count = Math.Floor(ch_element / Math.Pow(10, i));
+
+            element = ch_count - element_des;
+
+            element_des = ch_count * 10;
+
+            if (element > 0 && element <= 3)
+            {
+                for (int j = 0; j < element; j++) ch_string += Alfavit[6 - 2 * i];
+            }
+
+            if (element == 4)
+            {
+                ch_string += Alfavit[6 - 2 * i];
+                ch_string += Alfavit[5 - 2 * i];
+            }
+
+            if (element >= 5 && element <= 8)
+            {
+                ch_string += Alfavit[5 - 2 * i];
+                for (int j = 0; j < element - 5; j++) ch_string += Alfavit[6 - 2 * i];
+            }
+
+            if (element == 9)
+            {
+                ch_string += Alfavit[6 - 2 * i];
+                ch_string += Alfavit[6 - 2 * (i + 1)];
+            }
+        }
+        return ch_string;
+    }
+    //Реализация интерфейса IClonable
+    public object Clone()
+    {
+        return new RomanNumber(ch);
+    }
+    //Реализация интерфейса IComparable
+    public int CompareTo(object? obj)
+    {
+        if (obj is RomanNumber number)
+        {
+            return number.ch - ch;
+        }
+        else throw new ArgumentException("Неверные параметры");
+    }
+}
---
+++ VISUAL/lab_4/RomanNumberCalculator/Models/RomanNumberExtend.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class RomanNumberExtend : RomanNumber
+{
+    private static readonly Dictionary<char, int> pair =
+        new Dictionary<char, int> {
+            { 'I', 1 },
+            { 'V', 5 },
+            { 'X', 10 },
+            { 'L', 50 },
+            { 'C', 100 },
+            { 'D', 500 },
+            { 'M', 1000 }
+        };
+
+    public RomanNumberExtend(string num)  : base(1) {
+        int number = 0;
+        int pred_number = 0;
+        int count = 0;
+        bool check = true;
+        foreach (char ch in num)
+        {
+            if (check == true)
+            {
+                int curr = pair[ch];
+                if (number == curr) count++; else count = 0;
+
+                if (count >= 1 && ( curr == 5 || curr == 50 || curr == 500 )) check = false;
+                if (count >= 3 && (curr != 5 || curr != 50 || curr != 500)) check = false;
+
+                if (curr > number) pred_number += curr - 2 * number;
+                else pred_number += curr;
+                number = curr;
+            }
+            else throw new RomanNumberException("Неверная запись числа");
+        }
+        ch = Convert.ToUInt16(number);
+    }
+}
+
